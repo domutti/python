ff3e3b5b1fc194642d8f699960ead3f5c1dd3927
domutti <53964085+domutti@users.noreply.github.com>
2019-08-10 19:16:38 +0900

    Create Ex02_Quiz.py

---
+++ Ex02_Quiz.py
@@ -0,0 +1,15 @@
+name_1 = "유재석"
+birthday_1 = "1972/08/14"
+height_1 = 178
+weight_1 = 65
+job_1 = "개그맨"
+city_1 = "서울특별시"
+agency_1 = "FNC엔터"
+
+print("\n\n이름:", name_1, "\n생년월일:", birthday_1, "\n키:", height_1, "\n몸무게:", weight_1)
+print("------ 인물 정보 ------")
+print(name_1, job_1)
+print("생년월일", birthday_1,)
+print("출생지", city_1)
+print("신체 ", height_1, "cm ", weight_1, "kg", sep='')
+print("소속사", agency_1)
