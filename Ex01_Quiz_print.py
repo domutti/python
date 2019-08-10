0a198af6a2a166ce5997f19d7c2aef8565a06374
domutti <53964085+domutti@users.noreply.github.com>
2019-08-10 18:20:28 +0900

    Create Ex01_Quiz_print.py
    ez

---
+++ Ex01_Quiz_print.py
@@ -0,0 +1,16 @@
+# 본인의 이름, 나이, e-mail 주소를 출력하십시오
+print("이름: 김도현", "나이: 26", "e-mail: domutti1@gmail.com", sep=', ', end='\n\n')
+
+# 아래의 내용을 출력하십시오
+# --- 밀짚모자 해적단 ---
+# 이름        현상금
+# 루피        1200000
+# 초파        800000
+print("--- 밀짚모자 해적단 ---")
+print("이름", "현상금", sep='\t\t')
+print("루피", 1200000, sep='\t\t')
+print("초파", 800000, sep='\t\t', end='\n\n')
+# 파이썬 프로그램이 설치된 경로를 출력하십시오
+# "C:\Program Files\python\python36"
+print("\"", end='')
+print("C", "Program Files", "python", "python36", sep='\\', end='\"\n')
